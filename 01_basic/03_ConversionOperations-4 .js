d2b4cacc1e218cfafeaaee9ded74dbe3707984b8
sayandeepsoren19 <sorensayandeep@gmail.com>
2024-02-08 09:56:26 +0530

    Number and String Concatenation

---
+++ 01_basic/03_ConversionOperations-4 .js
@@ -0,0 +1,23 @@
+//$$$$$$$$$$$$$$$$$$$$$  NUMBER + STRING  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+console.log("sayandeep" + " soren");
+
+console.log(2 + "2");
+console.log("2" + 2);
+
+console.log(2 + 2 + "2");
+console.log("2" + 2 + 2);
+
+
+// $$$$$$$$$$$$$$$ PRE & POST INCREMENT OP.   $$$$$$$$$$$$$$$$$$$$$$$$$$$
+let x = 5;
+console.log(x++);//post-increment operator
+console.log(++x);//pre-increment operator
+
+
+// $$$$$$$$$$$$$$  USELESS THINGS   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+//NEVER USE THESE       --->        because, Readability is very important
+console.log(true);
+console.log(false);
+console.log(+true);
+console.log(+false);
