dee7fda6cd23eabb15b63ddf17477cf1641c2293
sayandeepsoren19 <sorensayandeep@gmail.com>
2024-02-17 10:43:47 +0530

    Scope level and mini hoisting in JS

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -1,8 +1,7 @@
 let x = 1;
 const y = 2;
 var z = 3;
-
-{
+{ //New Block Scope
   let x = 10;
   const y = 20;
   var z = 30;
@@ -11,7 +10,38 @@ var z = 3;
   console.log("Inner const: ",y);
   console.log("Inner var: ",z);
 }
-
 console.log("\nOuter let: ",x);
 console.log("Outer const: ",y);
 console.log("Outer var: ",z);   //Outer var has changed for Inner var
+console.log("----------------------111-----------------------------")
+
+function one(){
+    const userName = "Sayandeep";
+    
+    function two(){
+        const website = "Youtube";
+        console.log(userName);
+    }
+    
+    // console.log(website); //out of scope
+    
+    two();
+}
+
+one();
+console.log("----------------------222-----------------------------")
+
+//          Different Ways Of Declaring Functions
+
+console.log(addOne(5)); //Function Call before Declaration => ALLOWED
+function addOne(num){   //One Way to Declare function
+    return (num+1);
+}
+//-------------------------------------------------------------
+
+// console.log(addTwo(5));     //Function Call before Declaration => NOT ALLOWED
+const addTwo = function(num){ //Because of this way of function Declaration
+    return num+2;
+}
+console.log(addTwo(5));
+console.log("----------------------333-----------------------------")
